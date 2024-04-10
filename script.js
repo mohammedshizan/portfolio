816bb05061e087bc184ae93d5a2c8b2533c9ed8c
Mohammed Shizan <mohammedshizan411@gmail.com>
2024-04-10 12:36:44 +0530

    JS commit

---
+++ script.js
@@ -0,0 +1,26 @@
+var tabLinks = document.getElementsByClassName("tab-links"),
+tabContents = document.getElementsByClassName("tab-contents");
+
+function openTab(tabname) {
+    for (item of tabLinks) {
+        item.classList.remove("active-link");
+    }
+    for (item of tabContents) {
+        item.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+// Get SideMenu
+var SideMenu = document.getElementsByClassName("sidemenu");
+
+// On openMenu function call
+function openMenu(){
+    SideMenu.style.right = "0";
+}
+
+// On closeMenu function call
+function closeMenu(){
+    SideMenu.style.right = "-200px";
+}
