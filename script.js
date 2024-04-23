914afb28b848c7071563199a43bd48e29d41b0e2
Mohammed Shizan <mohammedshizan411@gmail.com>
2024-04-23 09:24:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
-var tabLinks = document.getElementsByClassName("tab-links"),
-tabContents = document.getElementsByClassName("tab-contents");
+const tabLinks = document.getElementsByClassName("tab-links");
+const tabContents = document.getElementsByClassName("tab-contents");
+const sideMenu = document.getElementById("sidemenu");
 
-function openTab(tabname) {
-    for (item of tabLinks) {
+function openTab(tabname, event) {
+    for (let item of tabLinks) {
         item.classList.remove("active-link");
     }
-    for (item of tabContents) {
+    for (let item of tabContents) {
         item.classList.remove("active-tab");
     }
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
 
-// Get SideMenu
-var SideMenu = document.getElementsByClassName("sidemenu");
-
-// On openMenu function call
-function openMenu(){
-    SideMenu.style.right = "0";
+function openMenu() {
+    if (sideMenu) {
+        sideMenu.style.right = "0";
+    }
 }
 
-// On closeMenu function call
-function closeMenu(){
-    SideMenu.style.right = "-200px";
+function closeMenu() {
+    if (sideMenu) {
+        sideMenu.style.right = "-200px";
+    }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Check if the element exists before adding event listener
+    const openMenuBtn = document.getElementById("openMenuBtn");
+    const closeMenuBtn = document.getElementById("closeMenuBtn");
+
+    if (openMenuBtn) {
+        openMenuBtn.addEventListener("click", openMenu);
+    }
+
+    if (closeMenuBtn) {
+        closeMenuBtn.addEventListener("click", closeMenu);
+    }
